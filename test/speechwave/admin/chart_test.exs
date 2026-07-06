1033f83584a5b969cd081bd8d0a638627be08027
Tracy Atteberry <tracy@magicbydesign.com>
2026-07-06 13:42:52 -0500

    test: assert chart SVG output differs for different history data
    Add a test that verifies Chart.render_svg/1 produces different SVG
    output for different input datasets, ensuring the function is
    data-driven rather than returning static output.

--- test/speechwave/admin/chart_test.exs
+++ test/speechwave/admin/chart_test.exs
@@ -20,4 +20,14 @@ defmodule Speechwave.Admin.ChartTest do
 
     assert html =~ "<svg"
   end
+
+  test "renders different SVG output for different history data" do
+    history_a = for i <- 29..0//-1, do: {Date.add(Date.utc_today(), -i), i}
+    history_b = for i <- 29..0//-1, do: {Date.add(~D[2020-01-01], -i), 100 - i}
+
+    svg_a = Chart.render_svg(history_a) |> Phoenix.HTML.Safe.to_iodata() |> IO.iodata_to_binary()
+    svg_b = Chart.render_svg(history_b) |> Phoenix.HTML.Safe.to_iodata() |> IO.iodata_to_binary()
+
+    refute svg_a == svg_b
+  end
 end
